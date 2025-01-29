90bd0824d64b2829a1e45c0b43bf4233c755e08b
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-01-30 00:48:39 +0530

    Added Latest Collections Page

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,15 +1,39 @@
-import React, {useContext} from 'react'
-import { ShopContext } from '../context/ShopContext'
-
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
-  const {products} = useContext(ShopContext);
-  
-    return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-export default LatestCollection
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="py-8 text-center text-3xl">
+        <Title text1={"LATEST"} text2={"COLLLECTIONS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-ggray-600">
+          Discover the latest trends with our newest collection! ✨ Shop
+          stylish, high-quality fashion designed to make you stand out. Refresh
+          your wardrobe today!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -5,7 +5,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = ( props ) => {
 
-    const currency = '₹';
+    const currency = '$';
     const delivery_fee = 10;
 
     const value = {
