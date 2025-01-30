ad69840428aaef0e0723718549763b79d76f2bdc
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-01-30 15:30:51 +0530

    Add Footer component to the application

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-1/2  text-gray-600">
+            At Sleek, we believe fashion is more than just clothing - it's a
+            statement. ✨ Our collections blend quality, style, and comfort to
+            bring you trend-forward pieces for every occasion. Elevate your
+            wardrobe with designs that stand out and speak confidence!
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91-8817090127</li>
+            <li>sleek@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ sleek.com - All Rights Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
