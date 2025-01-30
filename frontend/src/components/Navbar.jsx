0a13e651f6c0eda34c966764406932858c2f07c8
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-01-30 14:34:27 +0530

    Update in navbar's cart

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        <Link to="/cart" className="relative">
+        <Link to="/cart" onClick={() => setActiveLink('/cart')} className="relative">
           <img src={assets.cart_icon} alt="cart" className="w-5 min-w-5" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             10
