4fff21e39f9579db041510781807b9e62696411c
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-02-05 05:28:50 +0530

    Set up MongoDB and Cloudinary configurations, and implement product and user models

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,8 +4,9 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () => {
         console.log("MongoDB connected");
     });
+    
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 };
 
 export default connectDB;
