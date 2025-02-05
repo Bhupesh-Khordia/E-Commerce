bfa76008b750861889fdd4aaf4daf5d49c081db9
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-02-06 00:53:54 +0530

    Implement user authentication routes and controller functions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+const loginUser = (req, res) => {
+
+}
+
+const registerUser = (req, res) => {
+    
+}
+
+const adminLogin = (req, res) => {
+    
+}
+
+export {loginUser, registerUser, adminLogin}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoutes.js';
 
 // App Config
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // API Endpoints
+app.use('/api/user', userRouter);
 app.get('/', (req, res) => res.status(200).send('API Working!!'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
