841103a83a8c91ee10e3ae5b7b1b5bf355282528
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-02-06 09:56:34 +0530

    listProducts, removeProduct, singleProduct implemented.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -48,17 +48,37 @@ const addProduct = async (req, res) => {
 
 // Function to list Products
 const listProducts = async (req, res) => {
-    
+    try {
+        const products = await productModel.find({});
+        res.json({success : true, products});
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success : false, message : error.message});
+    }
 }
 
 // Function to remove product
 const removeProduct = async (req, res) => {
-    
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({success : true, message : "Product Deleted Successfully."})
+    } catch (error) {
+        console.log(error);
+        res.json({success : false, message : error.message});
+    }
 }
 
 // Function for single product info
 const singleProduct = async (req, res) => {
-    
+    try {
+        const {productId} = req.body;
+        const product = await productModel.findById(productId);
+        res.json({success : true, product})
+    } catch (error) {
+        console.log(error);
+        res.json({success : false, message : error.message});
+    }
 }
 
 export {addProduct, listProducts, removeProduct, singleProduct}
