d68d87d25b6d6f5492d961215a9d71f5d273007b
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-02-08 05:17:22 +0530

    Add order status update functionality in admin panel; implement status handler and backend update logic

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -29,6 +29,18 @@ const Orders = ({ token }) => {
     }
   };
 
+  const statusHandler = async (e, orderId) => {
+    try {
+      const response = await axios.post(backendUrl + '/api/order/status', {orderId, status : e.target.value}, {headers : {token}});
+      if(response.data.success) {
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
   useEffect(() => {
     fetchAllOrders();
   }, [token]);
@@ -88,7 +100,7 @@ const Orders = ({ token }) => {
               {currency}
               {order.amount}
             </p>
-            <select value={order.status} className="p-2 font-semibold">
+            <select onChange={(e) => statusHandler(e, order._id)} value={order.status} className="p-2 font-semibold">
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
