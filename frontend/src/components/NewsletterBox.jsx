57366b9a7fe597bfa5435953b47add092c342411
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-02-08 10:21:56 +0530

    Almighty Push

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+  import React from "react";
 
-const NewsletterBox = () => {
+  const NewsletterBox = () => {
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-    }
+      const handleFormSubmit = (event) => {
+          event.preventDefault();
+      }
 
-  return (
-    <div className="text-center">
-      <p className="text-2xl font-medium text-gray-800">
-        Subscribe Now and Get 20% Off !!
-      </p>
-      <p className="text-gray-400 mt-3">
-        Subscribe exclusive updates and special deals! 🎉 Be the first to know
-        about new arrivals and enjoy 20% off your first purchase.
-      </p>
-      <form action="" onSubmit={handleFormSubmit} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
-        <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter your Email" required />
-        <button type="submit" className="bg-black text-white text-xs px-10 py-4 cursor-pointer">SUBSCRIBE</button>
-      </form>
-    </div>
-  );
-};
+    return (
+      <div className="text-center">
+        <p className="text-2xl font-medium text-gray-800">
+          Subscribe Now and Get 20% Off !!
+        </p>
+        <p className="text-gray-400 mt-3">
+          Subscribe exclusive updates and special deals! 🎉 Be the first to know
+          about new arrivals and enjoy 20% off your first purchase.
+        </p>
+        <form action="" onSubmit={handleFormSubmit} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
+          <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter your Email" required />
+          <button type="submit" className="bg-black text-white text-xs px-10 py-4 cursor-pointer">SUBSCRIBE</button>
+        </form>
+      </div>
+    );
+  };
 
-export default NewsletterBox;
+  export default NewsletterBox;
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
-import NewsLetterbox from '../components/NewsLetterbox'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
@@ -35,7 +35,7 @@ const About = () => {
         <p className='text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
       </div>
     </div>
-    <NewsLetterbox />   
+    <NewsletterBox />   
   </div>  
   )
 }
